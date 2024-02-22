64881f92ae28bc44a2018963c58fd077e560d5eb
IAMLLT796 <lt.liu796@gmail.com>
2024-02-22 22:15:44 +0800

    cache实现并发特性

---
+++ cache.go
@@ -0,0 +1,38 @@
+package distcache
+
+import (
+	"distcache/lru"
+	"sync"
+)
+
+/*
+	并发属性
+*/
+
+type cache struct {
+	mu         sync.Mutex
+	lru        *lru.Cache
+	cacheBytes int64
+}
+
+func (c *cache) add(key string, value ByteView) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	// 延迟初始化，主要用于提高性能，并减少内存要求
+	if c.lru != nil {
+		c.lru = lru.New(c.cacheBytes, nil)
+	}
+	c.lru.Add(key, value)
+}
+
+func (c *cache) get(key string) (value ByteView, ok bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.lru == nil {
+		return
+	}
+	if v, ok := c.lru.Get(key); ok {
+		return v.(ByteView), ok
+	}
+	return
+}
