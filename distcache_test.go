d29a0807fa94f6030e375c0f0d11813933a9eadd
IAMLLT796 <lt.liu796@gmail.com>
2024-02-29 23:50:38 +0800

    实现函数回调和测试

---
+++ distcache_test.go
@@ -0,0 +1,18 @@
+package distcache
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGetter(t *testing.T) {
+	var f Getter = GetterFunc(func(key string) ([]byte, error) {
+		return []byte(key), nil
+	})
+
+	except := []byte("key")
+
+	if v, _ := f.Get("key"); !reflect.DeepEqual(v, except) {
+	}
+	t.Errorf("callback failed")
+}
