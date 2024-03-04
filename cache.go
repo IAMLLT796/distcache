c07cf1e5d91f7739cdc4f952f6dec5c62c5ce53c
IAMLLT796 <lt.liu796@gmail.com>
2024-03-04 23:19:14 +0800

    单机并发缓存及测试

--- cache.go
+++ cache.go
@@ -19,7 +19,7 @@ func (c *cache) add(key string, value ByteView) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	// 延迟初始化，主要用于提高性能，并减少内存要求
-	if c.lru != nil {
+	if c.lru == nil {
 		c.lru = lru.New(c.cacheBytes, nil)
 	}
 	c.lru.Add(key, value)
--- distcache.go
+++ distcache.go
@@ -1,8 +1,14 @@
 package distcache
 
+import (
+	"fmt"
+	"log"
+	"sync"
+)
+
 // Getter loads data for a key
 type Getter interface {
-	Get(key string) ([]byte, error)
+	Get(key string) ([]byte, error) // 回调函数
 }
 
 // GetterFunc implements Getter with a function
@@ -11,4 +17,70 @@ type GetterFunc func(key string) ([]byte, error)
 // Get implements Getter interface function
 func (f GetterFunc) Get(key string) ([]byte, error) {
 	return f(key)
+}
+
+// 一个 Group 可以认为是一个缓存命名空间，每个 Group 拥有一个唯一的名称 name
+type Group struct {
+	name      string // 每个 Group 拥有一个唯一的名称 name
+	getter    Getter // 缓存未命中时获取数据的回调(callback)
+	mainCache cache  // 一开始实现的并发缓存
+}
+
+var (
+	mu     sync.RWMutex
+	groups = make(map[string]*Group)
+)
+
+// 创建一个 Group 实例
+func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
+	if getter == nil {
+		panic("nil Getter")
+	}
+	mu.Lock()
+	defer mu.Unlock()
+
+	g := &Group{
+		name:      name,
+		getter:    getter,
+		mainCache: cache{cacheBytes: cacheBytes},
+	}
+	groups[name] = g
+
+	return g
+}
+
+// 获取指定名称的 Group
+func GetGroup(name string) *Group {
+	mu.RLock()
+	g := groups[name]
+	mu.RUnlock()
+	return g
+}
+
+func (g *Group) Get(key string) (ByteView, error) {
+	if key == "" {
+		return ByteView{}, fmt.Errorf("key is required")
+	}
+	if v, ok := g.mainCache.get(key); ok {
+		log.Println("[DistCache] hit")
+		return v, nil
+	}
+	return g.load(key)
+}
+
+func (g *Group) load(key string) (value ByteView, err error) {
+	return g.getLocally(key)
+}
+
+func (g *Group) getLocally(key string) (ByteView, error) {
+	bytes, err := g.getter.Get(key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	value := ByteView{b: cloneBytes(bytes)}
+	g.populateCache(key, value)
+	return value, nil
+}
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.add(key, value)
 }
