2e6f96f4b08d3560fbc4aebdfe5698244d1044f2
IAMLLT796 <lt.liu796@gmail.com>
2024-03-07 15:35:54 +0800

    实现 http 客户端

--- geecache/http.go
+++ geecache/http.go
@@ -1,10 +1,14 @@
 package geecache
 
 import (
+	"distcache/geecache/consistenthash"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"strings"
+	"sync"
 )
 
 /*
@@ -13,13 +17,19 @@ import (
 3.如果一个节点启动了 HTTP 服务，那么这个节点就可以被其他节点访问
 */
 
-const defaultBasePath = "/_geecache/"
+const (
+	defaultBasePath = "/_geecache/"
+	defaultReplicas = 50
+)
 
 // HTTPPool 实现了 PeerPicker 接口
 type HTTPPool struct {
 	// HTTP 节点之间的基础通信地址
-	self     string
-	basePath string
+	self        string
+	basePath    string
+	mu          sync.Mutex
+	peers       *consistenthash.Map    // 根据具体的 key 选择节点
+	httpGetters map[string]*httpGetter // 映射远程节点的 httpGetter, 每一个远程节点对应一个 httpGetter
 }
 
 // 构造函数
@@ -65,3 +75,48 @@ func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/octet-stream")
 	w.Write(view.ByteSlice())
 }
+
+type httpGetter struct {
+	baseURL string
+}
+
+func (p *HTTPPool) Set(peers ...string) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.peers = consistenthash.New(defaultReplicas, nil)
+	p.peers.Add(peers...)
+	p.httpGetters = make(map[string]*httpGetter, len(peers))
+	for _, peer := range peers {
+		p.httpGetters[peer] = &httpGetter{baseURL: peer + p.basePath}
+	}
+}
+func (p *HTTPPool) PickPeer(key string) (PeerGetter, bool) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	if peer := p.peers.Get(key); peer != "" && peer != p.self {
+		p.Log("Pick peer %s", peer)
+		return p.httpGetters[peer], false
+	}
+	return nil, true
+}
+
+func (h *httpGetter) Get(group string, key string) ([]byte, error) {
+	u := fmt.Sprintf("%v%v/%v", h.baseURL, url.QueryEscape(group), url.QueryEscape(key))
+	res, err := http.Get(u)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("server returned: %v", res.Status)
+	}
+
+	bytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("read response body: %v", err)
+	}
+	return bytes, nil
+}
+
+var _PeerGetter = (*httpGetter)(nil)
